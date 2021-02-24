7dc4e0aa03a7fead2e29a22feb0c1021a9979c47
Загир <43665070+zahirKulembetov@users.noreply.github.com>
2021-02-24 11:44:50 +0500

    Add files via upload

---
+++ dzsite3/js/style.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+   const hamburger = document.querySelector('.hamburger'),
+      menu = document.querySelector('.nav'),
+      closeMenu = document.querySelector('.nav__close'),
+      visible = document.querySelector('.visible');
+
+   hamburger.addEventListener('click', () => {
+      menu.classList.add('active');
+      visible.classList.add('active');
+   });
+
+   closeMenu.addEventListener('click', () => {
+      menu.classList.remove('active');
+      visible.classList.remove('active');
+   });
+
+   $('.main__slider').slick({
+      speed: 500,
+      prevArrow: $('.slick-prev'),
+      nextArrow: $('.slick-next'),
+      dots: true,
+      infinite: false,
+   });
+
+   $('.main__slider').on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+      let NumberSlide = nextSlide;
+      if (NumberSlide == 0) {
+         $('.slick-prev rect').css('opacity', "0.5");
+         $('.slick-prev path').css('opacity', "0.5");
+         $('.slick-next rect').css('opacity', "1");
+         $('.slick-next path').css('opacity', "1");
+      } else if (NumberSlide == 1) {
+         $('.slick-prev rect').css('opacity', "1");
+         $('.slick-prev path').css('opacity', "1");
+         $('.slick-next rect').css('opacity', "1");
+         $('.slick-next path').css('opacity', "1");
+      } else if (NumberSlide == 2) {
+         $('.slick-next rect').css('opacity', "0.5");
+         $('.slick-next path').css('opacity', "0.5");
+      }
+   });
+
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 400) {
+         $('.arrow').css('visibility', 'visible')
+         $('.arrow').fadeIn();
+      } else {
+         $('.arrow').fadeOut();
+      }
+   })
+
+   $(function () {
+      $("a[href='#head']").click(function () {
+         const _href = $(this).attr("href");
+         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+         return false;
+      });
+   });
+
+});
